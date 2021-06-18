fa80c7a2805e2a1e5d04e7124333104f709cb895
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-06-18 23:11:31 +0100

    commit 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const dummyTransactions = [
     {id: 1, name: 'Bolo de Arroz', amount: -20},
     {id: 2, name: 'Salário', amount: 300},
     {id: 3, name: 'Torta de frango', amount: -10},
-    {id: 4, name: 'Guitarra', amount: -20}
+    {id: 4, name: 'Guitarra', amount: 150}
 ]
 
 const addTransactionIntoDOM = transaction => {
@@ -21,8 +21,18 @@ const addTransactionIntoDOM = transaction => {
 }
 
 const updateBalanceValues = () => {
-    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount)
-    console.log(transactionAmounts)
+    const transactionAmounts = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionAmounts.filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    const expense = transactionAmounts.filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    console.log(expense)
 }
 
 const init = () =>{
